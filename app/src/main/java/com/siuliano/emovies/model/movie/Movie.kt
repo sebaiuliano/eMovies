f721da10511b114b97ede17c52a132d29deddb8f
sebastian-iuliano <sebastian.iuliano@mediamonks.com>
2022-07-31 12:46:33 -0300

    Add watch trailer logic

--- app/src/main/java/com/siuliano/emovies/model/movie/Movie.kt
+++ app/src/main/java/com/siuliano/emovies/model/movie/Movie.kt
@@ -18,7 +18,8 @@ data class Movie(
     val releaseDate: String,
     val title: String,
     @Json(name = "vote_average")
-    val voteAverage: Double
+    val voteAverage: Double,
+    val videos: Videos
 )
 
 @JsonClass(generateAdapter = true)
@@ -30,16 +31,30 @@ data class PaginatedMovieList(
 @JsonClass(generateAdapter = true)
 data class MovieMinimalData(
     val id: Int,
+    @Json(name = "genre_ids")
     val genres: List<Int>,
     val title: String,
     @Json(name = "release_date")
     val releaseDate: String,
     @Json(name = "poster_path")
-    val posterPath: String
+    val posterPath: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class Genre(
     val id: Int,
     val name: String
+)
+
+@JsonClass(generateAdapter = true)
+data class Videos(
+    val results: List<Video>
+)
+
+@JsonClass(generateAdapter = true)
+data class Video(
+    val key: String,
+    val site: String,
+    val type: String,
+    val official: Boolean
 )
